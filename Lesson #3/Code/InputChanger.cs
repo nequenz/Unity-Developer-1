f1ed774efa95cc7ff423c2ea3c34c9c2c4b0323e
Slava <70316676+nequenz@users.noreply.github.com>
2022-09-04 13:55:48 +0300

    Delete Lesson #3/Code directory

--- Lesson #3/Code/InputChanger.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class InputChanger : MonoBehaviour
-{
-    [SerializeField] private Health _health;
-    [SerializeField] private float _valueToAccept = 10f;
-
-    public void TakeValue() => _health?.TakeAmount(_valueToAccept);
-}
