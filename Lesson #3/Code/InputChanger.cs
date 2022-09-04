e734dcf9dd61773465e398d9bc2c8a6a91fcc5e9
Slava <70316676+nequenz@users.noreply.github.com>
2022-09-04 13:51:55 +0300

    Update InputChanger.cs

--- Lesson #3/Code/InputChanger.cs
+++ Lesson #3/Code/InputChanger.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class InputChanger : MonoBehaviour
 {
     [SerializeField] private Health _health;
-    [SerializeField] float _valueToAccept = 10f;
+    [SerializeField] private float _valueToAccept = 10f;
 
     public void TakeValue() => _health?.TakeAmount(_valueToAccept);
 }
