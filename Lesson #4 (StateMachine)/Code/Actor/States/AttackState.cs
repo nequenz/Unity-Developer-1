b73e1c81b490e1f187b3f45c8c4b706037e3d1d8
Slava <70316676+nequenz@users.noreply.github.com>
2022-09-24 16:10:00 +0300

    Update AttackState.cs

--- Lesson #4 (StateMachine)/Code/Actor/States/AttackState.cs
+++ Lesson #4 (StateMachine)/Code/Actor/States/AttackState.cs
@@ -3,33 +3,22 @@ using UnityEngine;
 [RequireComponent(typeof(AttackAbility))]
 public class AttackState : State
 {
-	[SerializeField] private State _winStateTarget;
-	[SerializeField] private Transform _target;
-	private AttackAbility _attackAbility;
+    [SerializeField] private State _winStateTarget;
+    private AttackAbility _attackAbility;
 
     protected override void Awake()
     {
-		base.Awake();
-		_attackAbility = GetComponent<AttackAbility>();
-	}
-
-    private void Start()
-    {
-		if( _target == null)
-        {
-			_target = FindObjectOfType<Health>().transform;
-		}
-
-		_attackAbility.SelectTarget(_target);
+        base.Awake();
+        _attackAbility = GetComponent<AttackAbility>();
     }
 
     private void Update()
-	{
-		if (_attackAbility.IsTargetExist() == true 
-			&& _attackAbility.TryGetTargetHealth(out Health health) 
-			&& health.CurrentHealth > 0)
-			_attackAbility.Attack();
-		else
-			ChangeState(_winStateTarget);
-	}
+    {
+        if (_attackAbility.IsTargetExist() == true
+            && _attackAbility.TryGetTargetHealth(out Health health)
+            && health.CurrentHealth > 0)
+            _attackAbility.Attack();
+        else
+            ChangeState(_winStateTarget);
+    }
 }
