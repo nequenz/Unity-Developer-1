e484500e6aec22706f67f0b57cec46ec7de5cee9
Slava <70316676+nequenz@users.noreply.github.com>
2022-09-24 16:09:20 +0300

    Update AttackAbility.cs

--- Lesson #4 (StateMachine)/Code/Actor/AttackAbility.cs
+++ Lesson #4 (StateMachine)/Code/Actor/AttackAbility.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AttackAbility : MonoBehaviour
@@ -9,37 +7,38 @@ public class AttackAbility : MonoBehaviour
     private float _delayAttack = 0f;
     private Transform _target;
 
-	private void Update()
-	{
-		if (_delayAttack > 0.0f)
-			_delayAttack -= Time.deltaTime;
-	}
-
-	private void CreateBullet()
-	{
-		Bullet bullet = Instantiate(_currentBullet, transform.position, Quaternion.identity);
-
-		bullet.SetAttacker(this);
-		bullet.SetTarget(_target);
-	}
-
-	public bool IsTargetExist() => _target != null;
-
-    public void SelectTarget( Transform transform ) => _target = transform;
-
-	public void Attack()
-	{
-		if (_delayAttack <= 0.0f && _target != null)
-		{
-			CreateBullet();
-			_delayAttack = _maxDelayAttack;
-		}
-	}
-
-	public bool TryGetTargetHealth(out Health health)
+    private void Update()
     {
-		health = null;
+        if (_delayAttack > 0.0f)
+            _delayAttack -= Time.deltaTime;
+    }
 
-		return (_target != null && _target.TryGetComponent(out health));
-	}
+    private void CreateBullet()
+    {
+        Bullet bullet = Instantiate(_currentBullet, transform.position, Quaternion.identity);
+
+        bullet.SetAttacker(this);
+        bullet.SetTarget(_target);
+    }
+
+    public bool IsTargetExist() => _target != null;
+
+    public void SelectTarget(Transform transform) => _target = transform;
+
+    public void Attack()
+    {
+        if (_delayAttack <= 0.0f && _target != null)
+        {
+            CreateBullet();
+            _delayAttack = _maxDelayAttack;
+        }
+    }
+
+    public bool TryGetTargetHealth(out Health health)
+    {
+        health = null;
+
+        return (_target != null && _target.TryGetComponent(out health));
+    }
+
 }
